81e8b32b9a21e40db4cefec34c9b16672169abee
litehouseadvisory <marc.vaneecke@litehouseadvisory.com>
2016-02-03 10:21:31 +0100

    corrected heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # required for heroku
-  config.action_mailer.default_url_options = { host: litehouse-pinteresting.herokuapp.com }
+  config.action_mailer.default_url_options = { host: 'litehouse-pinteresting.herokuapp.com' }
   
 end
