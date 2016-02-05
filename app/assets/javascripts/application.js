3f159899f9ffda23da2aabcb5e936f36edb858b8
litehouseadvisory <marc.vaneecke@litehouseadvisory.com>
2016-02-05 13:18:24 +0100

    styling sign up btn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap-sprockets
 
 //= require masonry/jquery.masonry
 //= require masonry/jquery.imagesloaded.min
